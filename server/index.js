683352c92313fc55dc6ece921e0cf5795f52428d
Dorothy Baker <tbakerdorothy@gmail.com>
2024-05-01 13:21:55 +0300

    redirecting

--- server/index.js
+++ server/index.js
@@ -7,10 +7,14 @@ import { connectToDB } from "./utils/connectToDB.js";
 import placeRouter from "./routes/place.route.js";
 import bookingRouter from "./routes/booking.route.js";
 
+import path from "path";
+
 config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +23,12 @@ app.use(cors({ credentials: true }));
 app.use("/api/auth", authRouter);
 app.use("/api/places", placeRouter);
 app.use("/api/bookings", bookingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   connectToDB();
