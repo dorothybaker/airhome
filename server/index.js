d34788094f409319d6f81baabfe9c72fbfd32199
Dorothy Baker <tbakerdorothy@gmail.com>
2024-05-01 13:10:11 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/places", placeRouter);
